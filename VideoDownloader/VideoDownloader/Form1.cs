17264b894aa1f94ca68f918bd9bc30cb2ed838ce
Anurag Bhattacharjee <anurag07061@gmail.com>
2016-09-17 23:17:06 +0600

    ading the project

---
+++ VideoDownloader/VideoDownloader/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            string sourceCode = WorkClass.getSourceCode(url);
+            StreamWriter sw = new StreamWriter("website.txt");
+            sw.Write(sourceCode);
+            sw.Close();
+        }
+    }
+}
